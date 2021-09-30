290f72b867caa8087a4a8819091fde82354e8b28
Nikhil Sharma <sharmanikhilm05@gmail.com>
2021-09-30 21:04:06 +0530

    Login Signup

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express=require('express');
-const fs=require('fs');
-const homepage=fs.readFileSync('./views/home.html','utf-8');
+const path=require('path');
 const app=express();
+const static_path = path.join(__dirname, "/public");
+app.set('view engine','hbs');
+app.use(express.static(static_path));
 app.get('/',(req,res)=>{
-    //res.header({'content-type':'text/html'});
-    res.set({
-        'Content-Type': 'text/html',
-      });
-    res.send(homepage);
+    res.render('home');
+});
+app.get('/login',(req,res)=>{
+    console.log('in login ');
+    res.render('login.hbs');
+});
+app.get('/signup',(req,res)=>{
+    res.render('signup');
 });
 app.listen(3000,()=>{
     console.log('server is running in http:localhost:3000');

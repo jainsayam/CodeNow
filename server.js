5d143b5438b2f3fa943931159defdf1dcdbd2ee8
Nikhil Sharma <sharmanikhilm05@gmail.com>
2021-10-09 17:02:59 +0530

    Shubham Changes

--- server.js
+++ server.js
@@ -1,55 +1,74 @@
-const express=require('express');
-const path=require('path');
+const express = require('express');
+const path = require('path');
+const bcrypt=require('bcrypt');
 require('./src/db/connection');
-const Register=require('./src/model/Register');
-const app=express();
+const Register = require('./src/model/register');
+const app = express();
 const static_path = path.join(__dirname, "/public");
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 app.use(express.static(static_path));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.get('/',(req,res)=>{
+app.use(express.urlencoded({ extended: false }));
+app.get('/', (req, res) => {
     res.render('home');
 });
-app.get('/home',(req,res)=>{
+app.get('/home', (req, res) => {
     res.render('home');
 });
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about');
 });
-app.get('/contactus',(req,res)=>{
+app.get('/contactus', (req, res) => {
     res.render('contactus');
 });
-app.get('/contest',(req,res)=>{
+app.get('/contest', (req, res) => {
     res.render('contests');
 });
-app.get('/login',(req,res)=>{
-    console.log('in login ');
+app.get('/login', (req, res) => {
     res.render('login.hbs');
 });
-app.get('/signup',(req,res)=>{
+app.get('/signup', (req, res) => {
     res.render('signup');
 });
-app.post('/register', async (req,res)=>{
-    try{
-        const password=req.body.password;
-        const cpassword=req.body.cpassword;
-        if(password === cpassword){
-            const registerUser=new Register({
-                name:req.body.name,
-                email:req.body.email,
-                password:req.body.password,
-                cpassword:req.body.cpassword
-            });
-            const registered=await registerUser.save();
-            res.status(201).render('home');
+app.post('/register', async (req, res) => {
+    try {
+        const password = req.body.password;
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const cpassword = req.body.cpassword;
+        if (req.body.name === '' || req.body.email === '' || password === '') {
+            res.send('Fields should not empty.');
         }else{
-            res.send('password are not matched');
+            if (password === cpassword) {
+                const registerUser = new Register({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashedPassword
+                });
+                const registered = await registerUser.save();
+                res.status(201).render('home');
+            } else {
+                res.send('password are not matched');
+            }
         }
-    }catch(err){
+
+    } catch (err) {
         res.status(400).send(err);
     }
 });
-app.listen(3000,()=>{
+app.post('/login',async (req,res)=>{
+    try{
+        const email=req.body.email,password=req.body.password;
+        const userDetail=await Register.findOne({email:email});
+        const matched=await bcrypt.compare(req.body.password, userDetail.password);
+        if(matched){
+            res.send('You are logged in.');
+        }else{
+            res.send('Invalid credentials');
+        }
+    }catch(error){
+        res.send('Invalid credentials');
+    }
+});
+app.listen(3000, () => {
     console.log('server is running in http:localhost:3000');
 });
--- src/model/register.js
+++ src/model/register.js
@@ -1,3 +1,4 @@
+const jwt=require('jsonwebtoken');
 const mongoose=require('mongoose');
 
 const usersScheme=new mongoose.Schema({
